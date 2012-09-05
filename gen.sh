c81f4f8b778b0de0a50c76967e20ccd4dab9c503
Tamás Szabó <tszabo@cae-engineering.hu>
2012-09-05 10:05:25 +0200

    Finetune preferences

--- gen.sh
+++ gen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-../../vendor/firebreath-dev/prepmac.sh projects build -D CMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_CXX_FLAGS="-DFB_NO_LOGGING_MACROS=1"
+firebreath/prepmac.sh projects build -D CMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_CXX_FLAGS="-DFB_NO_LOGGING_MACROS=1"
