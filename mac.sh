585831a8f2e389254d351d14ddf753abdccc8e49
Tamás Szabó <tszabo@cae-engineering.hu>
2012-09-05 16:17:34 +0200

    Finetune preferences 5

--- mac.sh
+++ mac.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-chmod a+x gen.sh
 ./gen.sh
-chmod a+x libffmpeg/configure
-cp libffmpeg/build-to-splugin.sh FFmpeg/build-to-splugin.sh
-chmod a+x FFmpeg/build-to-splugin.sh
-cd FFmpeg
-./build-to-splugin.sh libffmpeg/
+./libffmpeg/build-to-splugin.sh ./libffmpeg/
